df1b6f8b5c58a538265d4802e642b2714a173114
achay007 <akshayachz09@gmail.com>
2024-08-04 12:12:21 +0530

    spinner

--- fast pizza/src/uicomps/AppLayout.jsx
+++ fast pizza/src/uicomps/AppLayout.jsx
@@ -1,16 +1,23 @@
+/* eslint-disable */
 //import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Spinnger from "./Spinnger";
 function AppLayout() {
+  const navigation = useNavigation()
+  const isLoading = navigation.state === "loading"
   return (
-    <>
+    <div className='layout'>
+      {isLoading && <Spinnger/>}
       <Header />
+    
       <main>
         <Outlet />    
       </main>
+
       <CartOverview />
-    </>
+    </div>
   );
 }
 
---
+++ fast pizza/src/uicomps/Spinnger.jsx
@@ -0,0 +1,11 @@
+
+/* eslint-disable */
+import React from 'react'
+
+function Spinnger() {
+  return (
+    <div className='loader'></div>
+  )
+}
+
+export default Spinnger
