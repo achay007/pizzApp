bb0a98672ecf4e19582206cf62fa56232294b776
achay007 <akshayachz09@gmail.com>
2024-08-22 11:47:56 +0530

    style changes - tailwind

--- fast pizza/src/uicomps/Home.jsx
+++ fast pizza/src/uicomps/Home.jsx
@@ -1,11 +1,17 @@
+import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl text-yellow-500 font-semibold text-center">
+    <div className="my-10 sm:my-16 text-center  ">
+      <h1 className="mb-8 text-xl font-semibold text-center text-stone-700">
         The best pizza.
         <br />
+        <span className="text-yellow-500">
         Straight out of the oven, straight to you.
+        </span>
       </h1>
+
+      <CreateUser />
+
     </div>
   );
 }
