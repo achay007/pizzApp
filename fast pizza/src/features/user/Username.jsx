d326e18a9ba76e2da9387c3111e8208e3db5d995
achay007 <akshayachz09@gmail.com>
2024-12-22 12:40:13 +0530

    rebase2

--- fast pizza/src/features/user/Username.jsx
+++
@@ -1,9 +0,0 @@
-//eslint-disable-next-line
-import React from 'react'
-function Username() {
-  return (
-    <div className='text-sm font-semibold hidden  md:block'>Username</div>
-  )
-}
-
-export default Username
