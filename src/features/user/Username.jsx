e98e4d77c9ba9e936809556bbc2709bd0c46e4b0
achay007 <akshayachz09@gmail.com>
2024-12-25 10:59:50 +0530

    adding redux and updating userstate

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,11 @@
 //eslint-disable-next-line
 import React from 'react'
+import { useSelector } from 'react-redux'
 function Username() {
+  const username = useSelector(state => state.user.username)
+  console.log(username)
   return (
-    <div className='text-sm font-semibold hidden  md:block'>Username</div>
+    <div className='text-sm font-semibold hidden  md:block'>{username}</div>
   )
 }
 
